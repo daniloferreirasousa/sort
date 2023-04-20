096739c1026fc16c0ae4bfb157643f0b31ce2540
Danilo Ferreira <106565796+daniloferreirasousa@users.noreply.github.com>
2023-04-20 18:15:10 -0300

    Adição do dark Mode, Header e Footer

--- assets/js/sort.js
+++ assets/js/sort.js
@@ -1,6 +1,11 @@
+/**
+ * version: 1.0
+ * author:  Danilo Ferreira Sousa
+ */
 const form     = document.querySelector("form"),
       loader   = document.querySelector(".loader-area"),
-      reloader = document.querySelector(".reloader")
+      reloader = document.querySelector(".reloader"),
+      dark     = document.querySelector("#dark")
 
 var result = document.querySelector(".result"),
     title  = document.querySelector(".container-title")
@@ -22,7 +27,6 @@ form.addEventListener('submit', (event) => {
     loader.style.opacity = "1"
 
     setTimeout(() => {
-        
         form.style.display = "none"
         
         loader.style.opacity = "0"
@@ -38,7 +42,7 @@ form.addEventListener('submit', (event) => {
         reloader.style.display = "flex"
         reloader.style.opacity = "1"
         
-    }, 2500);
+    }, 2000);
 })
 
 reloader.addEventListener("click", () => {
@@ -47,5 +51,34 @@ reloader.addEventListener("click", () => {
 
 
 function sortear(min, max) {
-    return Math.floor(Math.random() * ( max - min + 1) + min);
+    return Math.ceil(Math.random() * (max - min + 1) + min)
 }
+
+
+/**
+ * Locais para tratar dark
+ * - header dark
+ * - div container dark-l
+ * - form dark
+ * - footer dark
+ */
+
+const header = document.querySelector("header"),
+      container = document.querySelector("div.container"),
+      footer = document.querySelector("footer")
+
+dark.addEventListener("click", () => {
+   if(header.classList.contains("dark") && container.classList.contains("dark-l") && form.classList.contains("dark") && footer.classList.contains("dark")) 
+   {
+    header.classList.remove("dark")
+    container.classList.remove("dark-l")
+    form.classList.remove("dark")
+    footer.classList.remove("dark")
+   }
+   else {
+        header.classList.add("dark")
+        container.classList.add("dark-l")
+        form.classList.add("dark")
+        footer.classList.add("dark")
+   }
+})
